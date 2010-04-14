2b44991ed3e436e43f8e8e6ece4235b6766336d9
John Harrison <ash@greaterthaninfinity.com>
2010-04-14 18:34:05 -0500

    Setting up the repository

---
+++ src/edu/auburn/csse/comp3710/AuburnTriviaGame.java
@@ -0,0 +1,13 @@
+package edu.auburn.csse.comp3710;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AuburnTriviaGame extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
