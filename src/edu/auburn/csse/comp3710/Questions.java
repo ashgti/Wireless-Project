51f182bc37124765015bd40fdae2db27f0966340
unknown <Logan@.(none)>
2010-04-15 10:47:14 -0500

    Updating things

--- src/edu/auburn/csse/comp3710/Questions.java
+++ src/edu/auburn/csse/comp3710/Questions.java
@@ -1,9 +1,12 @@
 package edu.auburn.csse.comp3710;
 
+import edu.auburn.csse.comp3710.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Questions extends Activity {
 	/** Called when the activity is first created. */
@@ -11,6 +14,7 @@ public class Questions extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.questions);
+        setCurrentQuestion();
     }
     
     public void correctAnswer(View view) {
@@ -19,4 +23,17 @@ public class Questions extends Activity {
     	setResult(Activity.RESULT_OK, resultIntent);
     	finish();
     }
+    
+    public void setCurrentQuestion() {
+    	//in future will be pulled from database
+    	Button correctAnswer = (Button)findViewById(R.id.Answer1);
+    	correctAnswer.setText("Gene Chizik");
+    	Button wrongAnswer1 = (Button)findViewById(R.id.Answer2);
+    	wrongAnswer1.setText("Tommy Tuberville");
+    	Button wrongAnswer2 = (Button)findViewById(R.id.Answer3);
+    	wrongAnswer2.setText("Will Muschamp");
+    	Button wrongAnswer3 = (Button)findViewById(R.id.Answer4);
+    	wrongAnswer3.setText("Trooper Taylor");
+	}
+    
 }
