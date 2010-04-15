312e5e842793f3ef4f3515943928ea434b47ecc3
John Harrison <ash@greaterthaninfinity.com>
2010-04-14 23:16:35 -0500

    Added ability to change between title context and question context.

--- src/edu/auburn/csse/comp3710/AuburnTriviaGame.java
+++ src/edu/auburn/csse/comp3710/AuburnTriviaGame.java
@@ -1,7 +1,9 @@
 package edu.auburn.csse.comp3710;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AuburnTriviaGame extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +11,24 @@ public class AuburnTriviaGame extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    public void newGame(View v) {
+    	Intent game = new Intent(this, Questions.class);
+    	startActivityForResult(game, 1);
+    }
+    
+    @Override 
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {     
+      super.onActivityResult(requestCode, resultCode, data); 
+      switch(requestCode) { 
+      	case (1) : { 
+      		if (resultCode == Activity.RESULT_OK) { 
+      			int tabIndex = data.getIntExtra("result", 0);
+      			// TODO Switch tabs using the index.
+      		} 
+      		break; 
+      	} 
+      } 
     }
 }
--- src/edu/auburn/csse/comp3710/Questions.java
+++ src/edu/auburn/csse/comp3710/Questions.java
@@ -1,7 +1,9 @@
 package edu.auburn.csse.comp3710;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Questions extends Activity {
 	/** Called when the activity is first created. */
@@ -9,5 +11,12 @@ public class Questions extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.questions);
+    }
+    
+    public void correctAnswer(View view) {
+    	Intent resultIntent = new Intent();
+    	resultIntent.putExtra("result", 1);
+    	setResult(Activity.RESULT_OK, resultIntent);
+    	finish();
     }
 }
