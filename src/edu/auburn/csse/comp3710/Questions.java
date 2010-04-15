f5e6e032673ef15fd9cebb0d527b2c1d36d1c2d8
John Harrison <ash@greaterthaninfinity.com>
2010-04-15 09:03:10 -0500

    Added a counter for the correct answer count.

--- src/edu/auburn/csse/comp3710/Questions.java
+++ src/edu/auburn/csse/comp3710/Questions.java
@@ -6,17 +6,23 @@ import android.os.Bundle;
 import android.view.View;
 
 public class Questions extends Activity {
+	int score;
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.questions);
+        score = 0;
+    }
+    
+    public void wrongAnswer(View view) {
+    	Intent resultIntent = new Intent();
+    	resultIntent.putExtra("result", score);
+    	setResult(Activity.RESULT_OK, resultIntent);
+    	finish();
     }
     
     public void correctAnswer(View view) {
-    	Intent resultIntent = new Intent();
-    	resultIntent.putExtra("result", 1);
-    	setResult(Activity.RESULT_OK, resultIntent);
-    	finish();
+    	score += 1;
     }
 }
