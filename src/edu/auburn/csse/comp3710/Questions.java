d7d4ae9a71ba06a960040b45fc4f8e56a07e4770
Logan <Logan@192.168.2.2>
2010-05-07 00:19:14 -0500

    create itetation through questions: gameplay

--- src/edu/auburn/csse/comp3710/Questions.java
+++ src/edu/auburn/csse/comp3710/Questions.java
@@ -12,16 +12,19 @@ import android.widget.TextView;
 public class Questions extends Activity {
 	int score;
 	public DataHelper QuestionDB;
+	int difficulty = 1;
+	int QuestionCount = 1;
 	
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.questions);
+        
         
         //initialize db
         QuestionDB = new DataHelper(this);
-        setCurrentQuestion();
+        setContentView(R.layout.questions);
+        nextQuestion(); 
         
         score = 0;
         
@@ -34,26 +37,73 @@ public class Questions extends Activity {
     	finish();
     }
     
-    public void setCurrentQuestion() {
-    	//String test = QuestionDB.selectQuestion(QuestionTypes.AU, 1);
-    	String[] currentQuestion = QuestionDB.selectQuestion(QuestionTypes.AU, 1);
-    		
+    public void setCurrentQuestion(int difficulty) {
+    	Question currentQuestion = new Question(QuestionDB, difficulty);
+    	String[] WrongAnswers = currentQuestion.getWrongAnswers();
     	//TODO: Dynamically change order so first answer is not always the correct answer 
     	
+    	try{
     	TextView question = (TextView)findViewById(R.id.Question);
-    	question.setText(currentQuestion[0]);
+    	question.setText(currentQuestion.getQuestion());
     	Button correctAnswer = (Button)findViewById(R.id.Answer1);
-    	correctAnswer.setText(currentQuestion[1]);
+    	correctAnswer.setText(currentQuestion.getCorrectAnswer());
     	Button wrongAnswer1 = (Button)findViewById(R.id.Answer2);
-    	wrongAnswer1.setText(currentQuestion[2]);
+    	wrongAnswer1.setText(WrongAnswers[0]);
     	Button wrongAnswer2 = (Button)findViewById(R.id.Answer3);
-    	wrongAnswer2.setText(currentQuestion[3]);
+    	wrongAnswer2.setText(WrongAnswers[1]);
     	Button wrongAnswer3 = (Button)findViewById(R.id.Answer4);
-    	wrongAnswer3.setText(currentQuestion[4]);
+    	wrongAnswer3.setText(WrongAnswers[2]);
+    	}
+    	catch(Exception EX)
+    	{
+    		System.out.println(EX);
+    	}
 	}
     
     public void correctAnswer(View view) {
     	score += 1;
+    	nextQuestion();
+    }
+    
+    public void nextQuestion()
+    {
+    	
+    	if(QuestionCount < 13)
+    	{
+    		
+    	difficulty = QuestionCount/3;
+    	
+    	if(QuestionCount%3 != 0)
+    	{
+    		difficulty++;
+    	}
+    		
+    	switch (difficulty) 
+    	{
+			case 1:
+				setCurrentQuestion(1);
+				break;
+
+			case 2:
+				setCurrentQuestion(2);
+				break;
+	
+			case 3:
+				setCurrentQuestion(3);
+				break;
+
+			case 4:
+				setCurrentQuestion(4);
+				break;
+
+			
+			default:
+				break;
+		}
+    	
+    	QuestionCount++;
+    	
+    	}
     }
 
 }
